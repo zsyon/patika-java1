0d9c642606425e802797267aadfebcaba872291f
HP <20060339@stu.omu.edu.tr>
2023-02-28 22:11:40 +0300

    add math

---
+++ notOrtHesaplama.java
@@ -0,0 +1,17 @@
+/**
+ * notOrtHesaplama
+ */
+import java.util.Scanner;
+
+public class notOrtHesaplama {
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int mat;
+
+    System.out.println("Matematik notunu giriniz :");
+    mat = input.nextInt();
+    System.out.println(mat);
+  }
+}
