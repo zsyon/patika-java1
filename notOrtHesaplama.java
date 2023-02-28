5e9ad1cf2cd9f97cb5eda8bcdc760531ba70c8b1
HP <20060339@stu.omu.edu.tr>
2023-02-28 22:21:42 +0300

    add lesson

--- notOrtHesaplama.java
+++ notOrtHesaplama.java
@@ -2,16 +2,32 @@
  * notOrtHesaplama
  */
 import java.util.Scanner;
+import java.util.SortedMap;
 
 public class notOrtHesaplama {
   public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
 
-    int mat;
+    int mat ,fizik ,kimya ,turce, tarih, muzuk ,ort;
 
-    System.out.println("Matematik notunu giriniz :");
+    System.out.print("Matematik Notu :");
     mat = input.nextInt();
-    System.out.println(mat);
+
+    System.out.print("Fizik Notu :");
+    fizik = input.nextInt();
+
+    System.out.print("Kimya Notu :");
+    kimya = input.nextInt();
+
+    System.out.print("Türkçe Notu :");
+    turce = input.nextInt();
+
+    System.out.print("Tarih Notu :");
+    tarih = input.nextInt();
+
+    System.out.print("Müzik Notu :");
+    muzuk = input.nextInt();
+
   }
 }
