2e2d27eba8e354ee2e262513f96d87953a330751
HP <20060339@stu.omu.edu.tr>
2023-03-01 00:41:48 +0300

    add

---
+++ kitleİndeksiHesaplama.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class kitleİndeksiHesaplama{
+  public static void main(String[] args) {
+    double boy, kilo;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+    boy = input.nextDouble();
+
+    System.out.print("Lütfen kilonuzu giriniz : ");
+    kilo = input.nextDouble();
+
+    double kitleİndeksi = kilo / (boy * boy);
+    System.out.println("Vücut Kitle İndeksiniz : " + kitleİndeksi);
+  }
+  
+}
