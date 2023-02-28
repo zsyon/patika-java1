e5f72b58310c51f61bef63ece0c86699d4553ba9
HP <20060339@stu.omu.edu.tr>
2023-02-28 23:31:44 +0300

    set the Kdv rate

--- kdvHesaplama.java
+++ kdvHesaplama.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class kdvHesaplama {
   public static void main(String[] args) {
 
-    double tutar, Kdv = 0.18;
+    double tutar, Kdv;
     Scanner input = new Scanner(System.in);
 
     System.out.print("Ücret Tutarını Giriniz : ");
     tutar = input.nextDouble();
 
-    double kdvli = (Kdv*tutar) + tutar;
+    //double kdvli = (Kdv*tutar) + tutar;
+
+    boolean sonuc = tutar > 1000;
+
+    double kdvli = sonuc ? (0.08*tutar) + tutar : (0.18*tutar) + tutar;
+
     double kdvTutari = kdvli - tutar;
 
     System.out.println("KDV'siz Fiyat : " + tutar);
     System.out.println("KDV'li Fiyat : " + kdvli);
     System.out.println("KDV Tutarı : " + kdvTutari);
-
-
-
     
   }
   
