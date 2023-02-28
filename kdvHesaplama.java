1adbed469c1607148c38b64b251c05e40a6ee2e9
HP <20060339@stu.omu.edu.tr>
2023-02-28 23:13:28 +0300

    add money

---
+++ kdvHesaplama.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class kdvHesaplama {
+  public static void main(String[] args) {
+
+    double tutar, Kdv = 0.18;
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Ücret Tutarını Giriniz : ");
+    tutar = input.nextDouble();
+
+    System.out.print("KDV'siz Fiyat : " + tutar);
+
+
+
+    
+  }
+  
+}
